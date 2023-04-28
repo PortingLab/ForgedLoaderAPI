8787c9e781ec1ecab0bfcf43b3790f180847ea11
TexTrue <3140846162@qq.com>
2023-04-28 13:48:41 +0800

    slf4j? log4j!

--- src/main/java/org/portinglab/forgedfabric/loader/ForgedFabricLoader.java
+++ src/main/java/org/portinglab/forgedfabric/loader/ForgedFabricLoader.java
@@ -1,14 +1,14 @@
 package org.portinglab.forgedfabric.loader;
 
 import net.minecraftforge.fml.common.Mod;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Mod(ForgedFabricLoader.MODID)
 public class ForgedFabricLoader {
     public static final String MODID = "forgedfabricloader";
     public static final String MODNAME = "ForgedFabricLoaderAPI";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MODNAME);
+    public static final Logger LOGGER = LogManager.getLogger(MODNAME);
 
     public ForgedFabricLoader() {
 
